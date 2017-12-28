d0589c21b4224b39a173bd57ef8297a247253413
Lasse Rafn <lasserafn@gmail.com>
2017-12-28 13:28:59 +0100

    Only display a single incident

--- app/Listeners/WhenMonitorsFail.php
+++ app/Listeners/WhenMonitorsFail.php
@@ -4,20 +4,27 @@ namespace CachetHQ\Cachet\Listeners;
 
 use CachetHQ\Cachet\Bus\Commands\Incident\CreateIncidentCommand;
 use CachetHQ\Cachet\Models\Component;
+use CachetHQ\Cachet\Models\Incident;
 use CachetHQ\Cachet\Models\User;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Spatie\UptimeMonitor\Events\UptimeCheckFailed;
 
 class WhenMonitorsFail
 {
-	public function handle( UptimeCheckFailed $event )
-	{
+	public function handle( UptimeCheckFailed $event ) {
+		/** @var Component $component */
 		$component = Component::whereLink( $event->monitor->url )->first();
 
-		if ( $component )
-		{
+		if ( $component ) {
+			/** @var Incident $latestIncident */
+			if ( (int) $component->status === 2 && $latestIncident = $component->incidents()->whereName( 'Uptime Monitor' )->whereStatus( 2 )->latest()->first() ) {
+				if ( $latestIncident->message === ( $event->monitor->uptime_check_failure_reason ?? '' ) ) {
+					return;
+				}
+			}
+
 			Auth::login( User::first() );
+
 			dispatch( new CreateIncidentCommand(
 				'Uptime Monitor',
 				2,
