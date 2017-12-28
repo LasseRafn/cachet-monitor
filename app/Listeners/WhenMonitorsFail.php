bd1e0aa25870dbc2f9ef910f7ee955ef034e4ace
Lasse Rafn <lasserafn@gmail.com>
2017-12-28 13:52:39 +0100

    Undo

--- app/Listeners/WhenMonitorsFail.php
+++ app/Listeners/WhenMonitorsFail.php
@@ -16,13 +16,6 @@ class WhenMonitorsFail
 		$component = Component::whereLink( $event->monitor->url )->first();
 
 		if ( $component ) {
-			/** @var Incident $latestIncident */
-			if ( (int) $component->status === 2 && $latestIncident = $component->incidents()->whereNull('deleted_at')->whereName( 'Uptime Monitor' )->whereStatus( 2 )->latest()->first() ) {
-				if ( $latestIncident->message === ( $event->monitor->uptime_check_failure_reason ?? '' ) ) {
-					return;
-				}
-			}
-
 			Auth::login( User::first() );
 
 			dispatch( new CreateIncidentCommand(
